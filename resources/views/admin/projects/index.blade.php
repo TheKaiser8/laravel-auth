5c77b893db6ad1543fb5e8de996107acd1c5f524
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-01-25 19:23:11 +0100

    Aggiunge stampa tabella projects in index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,4 +2,24 @@
 
 @section('content')
     <h1 class="text-center">Lista progetti</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+            <tr>
+                <td>{{ $project->id }}</td>
+                <td>{{ $project->title }}</td>
+                <td>{{ $project->slug }}</td>
+                <td></td>
+            </tr>
+            @endforeach
+        </tbody>
+      </table>
 @endsection
